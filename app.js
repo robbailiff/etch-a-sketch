d6c71b4237b2d6f4a8eeaa106b65d3ef10fbfbaa
Rob Bailiff <robbailiff@gmail.com>
2020-12-02 21:06:33 +0000

    Changes to positioning of page elements

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".grid-container")
 let gridSize = 16;
 
 for (let i = 1; i <= gridSize**2; i++) {
     const newCell = document.createElement("div");
-    newCell.classList.add("grid");
+    newCell.classList.add("grid-cell");
     container.append(newCell);
 };
+
+const cells = document.querySelectorAll(".grid-cell");
+cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
+    	console.log(e);
+        e.target.style.backgroundColor = "red";
+    })
+})
+     
+     
